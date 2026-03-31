f7815a7bd5a2fa0eb7a78a460a4e0dd1457f9c71
Fikri M Tech <muh44madfikri@gmail.com>
2026-03-31 09:24:07 +0700

    Update gallery captions in Home.py

--- Home.py
+++ Home.py
@@ -112,12 +112,12 @@ with col1:
 with col2:
     st.markdown('<div class="gallery-card">', unsafe_allow_html=True)
     st.image("assets/foto2.jpg", use_container_width=True)
-    st.markdown('<div class="gallery-caption">Ketua Rabi bolo</div></div>', unsafe_allow_html=True)
+    st.markdown('<div class="gallery-caption">Seragam</div></div>', unsafe_allow_html=True)
 
 with col3:
     st.markdown('<div class="gallery-card">', unsafe_allow_html=True)
     st.image("assets/foto3.jpg", use_container_width=True)
-    st.markdown('<div class="gallery-caption">Kegiatan 3</div></div>', unsafe_allow_html=True)
+    st.markdown('<div class="gallery-caption">Anggota</div></div>', unsafe_allow_html=True)
 
 # Baris 2
 col4, col5, col6 = st.columns(3)
@@ -125,7 +125,7 @@ col4, col5, col6 = st.columns(3)
 with col4:
     st.markdown('<div class="gallery-card">', unsafe_allow_html=True)
     st.image("assets/foto4.jpg", use_container_width=True)
-    st.markdown('<div class="gallery-caption">Kegiatan 4</div></div>', unsafe_allow_html=True)
+    st.markdown('<div class="gallery-caption">Ketua Rabi boloo</div></div>', unsafe_allow_html=True)
 
 with col5:
     st.markdown('<div class="gallery-card">', unsafe_allow_html=True)
